11251f089e98dc17ffa35dc2cc844c21834bc911
imrantaj1974 <imrantaj@hotmail.com>
2024-03-09 06:26:21 +0500

    prep2 input taken

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade");
+        int gradeReceived = Console.ReadLine();
+        Console.WriteLine($"The grade you entered is" {gradeReceived});
     }
 }
