0a094d088ec78547bc6ea4950d15b8295652b25e
imrantaj1974 <imrantaj@hotmail.com>
2024-03-09 06:31:12 +0500

    int added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,7 +2,9 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    /*static void Main(string[] args)*/
+    static void Main(int[] args)
+    /* I just want to make sure that int arguments are being passed*/
     {
         Console.WriteLine("Hello Prep2 World!");
         Console.WriteLine("Enter your grade");
