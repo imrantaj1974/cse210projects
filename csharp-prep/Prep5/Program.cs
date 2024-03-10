b635e4129fb4761048a85df6a2e4bbf462e2bead
imrantaj1974 <imrantaj@hotmail.com>
2024-03-10 11:58:38 +0500

    A function added in the program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Configuration.Assemblies;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+
+        Void WriteMessage()
+        {
+            Console.WriteLine("This is a simply display message");
+        }
+
     }
 }
