9b3c1f878c2b191dd201709c667b55a1f057b768
imrantaj1974 <imrantaj@hotmail.com>
2024-03-15 16:57:44 +0500

    Display methods replaced by resume Class

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,12 +2,15 @@ public class Job
 {
     public string _jobTitle = "";
     public string _companyName="";
-    public string _companyYear="";
+    // public string _companyYear="";
+    public int _companyStartYear=2019;
+    public int _companyEndYear=2022;
+
 
     
 
     public void Display()
     {
-        Console.WriteLine($"{_jobTitle} ({_companyName}) {_companyYear}");
+        Console.WriteLine($"{_jobTitle} ({_companyName}) {_companyStartYear}-{_companyEndYear}");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,23 +12,28 @@ class Program
 
         job1._jobTitle = "Software Engineer";
         job1._companyName = "MicroSoft";
-        job1._companyYear = "2019-2022";
-
+        // job1._companyYear = "2019-2022";
+        job1._companyStartYear = 2019;
+        job1._companyEndYear = 2022;
 
         job2._jobTitle = "Manager";
         job2._companyName = "Apple";
-        job2._companyYear = "2022-2023";
+        job2._companyStartYear = 2022;
+        job2._companyEndYear = 2023;
 
-
+        // job2._companyYear = "2022-2023";
 
         // Console.WriteLine($"{job1._jobTitle}");
         // Console.WriteLine($"{job2._jobTitle}");
-        
-        job1.Display();
+        Resume mineResume = new Resume();
+        mineResume._name = "Allison Rose";
+        mineResume._jobs.Add(job1);
+        mineResume._jobs.Add(job2);
+        mineResume.Display();
 
+        // job1.Display();
 
         // job2.Display();
-
 
         
     }
